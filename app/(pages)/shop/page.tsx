2edfee3086a29c94252fcf119de18f8d63967a77
Mythicalx <creators462023@gmail.com>
2024-06-23 17:05:55 +0530

    added coming soon

---
+++ app/(pages)/shop/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import SEO from '@/components/SEO';
+import { Passion_One } from 'next/font/google';
+
+const passion = Passion_One({ subsets: ['latin'], weight: '400', })
+
+
+function Privacy() {
+
+    return (
+        <main className="flex flex-col min-h-screen w-screen bg-[#F3F4F5] overflow-x-hidden p-4">
+            <div>
+                Shop Coming soon...
+            </div>
+        </main>
+    )
+}
+
+export default Privacy;
