021d7b71ca64658a8ab6489fd4562669cb3f533c
Mythicalx <creators462023@gmail.com>
2024-07-06 17:11:29 +0530

    fixed offerwall

--- app/(pages)/offerwall/page.tsx
+++ app/(pages)/offerwall/page.tsx
@@ -10,7 +10,7 @@ function About() {
 
     return (
         <main className="flex flex-col min-h-screen w-screen bg-[#F3F4F5] overflow-x-hidden">
-            <iframe src='https://notik.me/coins?api_key=rqHZrGYq2z2bJQ9QozKLkEtBwhMxtr32&pub_id=8kUzVg&app_id=8ZfM39Uzu5&user_id={YOUR_UNIQUE_USER_ID}'></iframe>
+            <iframe src='https://notik.me/coins?api_key=rqHZrGYq2z2bJQ9QozKLkEtBwhMxtr32&pub_id=8kUzVg&app_id=8ZfM39Uzu5&user_id={YOUR_UNIQUE_USER_ID}' className='h-screen' ></iframe>
         </main >
     )
 }
