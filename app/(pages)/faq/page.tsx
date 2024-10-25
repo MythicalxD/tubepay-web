d826668c971cc105645ea529dccf369e83db596f
Mythicalx <creators462023@gmail.com>
2024-10-25 17:20:49 +0530

    added faq

---
+++ app/(pages)/faq/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import SEO from '@/components/SEO';
+import { Passion_One } from 'next/font/google'
+import Link from 'next/link';
+
+const passion = Passion_One({ subsets: ['latin'], weight: '400', })
+
+
+function Privacy() {
+
+    return (
+        <main className="flex flex-col min-h-screen w-screen bg-[#F3F4F5] overflow-x-hidden">
+            <SEO
+                title="Terms and Conditions - Tubepay"
+                description="tubepay tnc"
+                canonical="https://tubepay.live/tnc"
+            />
+
+            <div className="flex h-[70px] items-center px-[15px] w-screen bg-gradient-to-r from-orange-400 to-pink-500">
+                <a href="https://play.google.com/store/apps/details?id=com.earn.tubepay.tubepay">
+                    <img src="./logo.png" alt="logo" className='h-[40px]' />
+                </a>
+                <Link href="/">
+                    <p className='text-md font-bold text-gray-700 hover:text-black ml-4' >HOME</p>
+                </Link>
+            </div>
+            <div className="text-black p-8">
+                <h1 className='text-3xl' ><strong>TubePay Frequently Asked Questions</strong></h1><br></br>
+                <p>
+                   COMING SOON...
+                </p>
+
+            </div>
+        </main>
+    )
+}
+
+export default Privacy;
